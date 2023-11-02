2c2605da5fb4749c4d4d8ba4bc61d875cf5923d3
Katharina Dröge <kate@commandmc.de>
2023-11-02 01:29:56 +0100

    Make it possible to sync a folder between Stud.IP and the local file system
    This is only able to download files for now, since that's all I need it to do

--- src/renderer/src/pages/CoursePage/index.tsx
+++ src/renderer/src/pages/CoursePage/index.tsx
@@ -1,3 +1,4 @@
+import type { ReactNode } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 import Box from '@mui/material/Box'
@@ -7,12 +8,17 @@ import AccordionDetails from '@mui/material/AccordionDetails'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
 import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import CommentIcon from '@mui/icons-material/Comment'
 import VisibilityIcon from '@mui/icons-material/Visibility'
 import FolderIcon from '@mui/icons-material/Folder'
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile'
+import DownloadingIcon from '@mui/icons-material/Downloading'
+import DownloadDoneIcon from '@mui/icons-material/DownloadDone'
+import DeleteIcon from '@mui/icons-material/Delete'
+import LoopIcon from '@mui/icons-material/Loop'
 
 import type { CourseMetadata, File, Folder } from '../../../../main/api/interfaces.ts'
 import useUserState from '../../state.ts'
@@ -154,11 +160,20 @@ function FolderWidget({ folder, on_folder_click }: FolderWidgetProps) {
 
 interface CourseFilesWidgetProps {
     files: Folder['contents'] | false | undefined
+    course_id: string
 }
-function CourseFilesWidget({ files }: CourseFilesWidgetProps) {
+function CourseFilesWidget({ files, course_id }: CourseFilesWidgetProps) {
     const [selected_files_stack, set_selected_files_stack] = useState<{ label: string; files: Folder['contents'] }[]>(
         []
     )
+    const [synchronized_folder, set_synchronized_folder] = useState<string | false | undefined>(undefined)
+    const [is_synchronizing, set_is_synchronizing] = useState(false)
+
+    useEffect(() => {
+        const configured_sync_folder = window.localStorage.getItem(`${course_id}_file_sync`) ?? false
+        set_synchronized_folder(configured_sync_folder)
+        if (configured_sync_folder) on_sync_button_click(configured_sync_folder)
+    }, [])
 
     useEffect(() => {
         if (files)
@@ -190,33 +205,104 @@ function CourseFilesWidget({ files }: CourseFilesWidgetProps) {
         [selected_files_stack]
     )
 
+    const on_add_sync_button_click = useCallback(async () => {
+        const current_course_obj = (useUserState.getState().courses || []).find((course) => course.id === course_id)
+        if (!current_course_obj) return
+        const selected_folder = await IPC.select_sync_folder(current_course_obj.name)
+        if (!selected_folder) return
+        window.localStorage.setItem(`${course_id}_file_sync`, selected_folder)
+        set_synchronized_folder(selected_folder)
+        on_sync_button_click(selected_folder)
+    }, [])
+
+    const on_sync_button_click = useCallback((sync_folder: string) => {
+        if (!files) return
+        set_is_synchronizing(true)
+        IPC.sync_folder(files, sync_folder).then(() => set_is_synchronizing(false))
+    }, [])
+
+    const on_remove_sync_button_click = useCallback(() => {
+        set_synchronized_folder(false)
+    }, [])
+
     if (files === false) return <Paper>Failed to fetch files</Paper>
     if (files === undefined) return <LoadingComponent />
+    if (synchronized_folder === undefined) return <LoadingComponent />
 
     const currently_displayed_filelist = selected_files_stack.at(-1)?.files
     if (!currently_displayed_filelist) return <LoadingComponent />
 
     return (
         <Paper>
-            <Stack direction='row'>
-                Currently selected:
-                <Stack sx={{ marginLeft: 1, marginBottom: 2 }} direction='row'>
-                    {selected_files_stack
-                        .map<React.ReactNode>(({ label }, index) => (
-                            <Box key={index} sx={{ cursor: 'pointer' }} onClick={() => on_history_click(index)}>
-                                {label}
-                            </Box>
-                        ))
-                        .reduce((prev, curr) => [prev, ' / ', curr])}
+            <Stack spacing={2}>
+                <Stack direction='row'>
+                    Currently selected:
+                    <Stack sx={{ marginLeft: 1 }} direction='row'>
+                        {selected_files_stack
+                            .map<ReactNode>(({ label }, index) => (
+                                <Box key={index} sx={{ cursor: 'pointer' }} onClick={() => on_history_click(index)}>
+                                    {label}
+                                </Box>
+                            ))
+                            .reduce((prev, curr) => [prev, ' / ', curr])}
+                    </Stack>
                 </Stack>
-            </Stack>
-            <Stack>
-                {currently_displayed_filelist.folders.map((folder) => (
-                    <FolderWidget key={folder.id} folder={folder} on_folder_click={on_folder_click} />
-                ))}
-                {currently_displayed_filelist.files.map((file) => (
-                    <FileWidget key={file.id} file={file} on_file_click={on_file_click} />
-                ))}
+                <Stack>
+                    {currently_displayed_filelist.folders.map((folder) => (
+                        <FolderWidget key={folder.id} folder={folder} on_folder_click={on_folder_click} />
+                    ))}
+                    {currently_displayed_filelist.files.map((file) => (
+                        <FileWidget key={file.id} file={file} on_file_click={on_file_click} />
+                    ))}
+                </Stack>
+                <Stack direction='row'>
+                    {!synchronized_folder && (
+                        <>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                Not synchronized
+                            </Box>
+                            <Box flexGrow={1} />
+                            <Button color='success' onClick={on_add_sync_button_click}>
+                                Synchronize
+                            </Button>
+                        </>
+                    )}
+                    {synchronized_folder && (
+                        <>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                {is_synchronizing ? <DownloadingIcon /> : <DownloadDoneIcon />}
+                                Synchronizing to {synchronized_folder}
+                            </Box>
+                            <Box flexGrow={1} />
+                            <Button
+                                onClick={() => on_sync_button_click(synchronized_folder)}
+                                disabled={is_synchronizing}
+                                startIcon={<LoopIcon />}
+                            >
+                                Sync now
+                            </Button>
+                            <Button
+                                onClick={on_remove_sync_button_click}
+                                disabled={is_synchronizing}
+                                startIcon={<DeleteIcon />}
+                            >
+                                Remove sync (keeps files)
+                            </Button>
+                        </>
+                    )}
+                </Stack>
             </Stack>
         </Paper>
     )
@@ -268,7 +354,7 @@ function CoursePage() {
                     <Typography variant='h6' sx={{ marginTop: 3 }}>
                         Files:
                     </Typography>
-                    <CourseFilesWidget files={course_files} />
+                    <CourseFilesWidget files={course_files} course_id={course_id} />
                 </>
             )}
         </Box>
