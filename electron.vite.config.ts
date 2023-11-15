6504d9a0f7633f4050b979ea1d13e050e9e09d75
Katharina Dröge <kate@commandmc.de>
2023-11-15 15:57:59 +0100

    Minify production builds

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     main: {
+        build: {
+            minify: mode === 'production'
+        },
         plugins: [externalizeDepsPlugin()]
     },
     preload: {
+        build: {
+            minify: mode === 'production'
+        },
         plugins: [externalizeDepsPlugin()]
     },
     renderer: {
+        build: {
+            minify: mode === 'production'
+        },
         plugins: [react()]
     }
-})
+}))
