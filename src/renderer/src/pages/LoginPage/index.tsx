c7ab7d6491207152f42257f2e9fec75885c38416
Mathis Dröge <mathis.droege@ewe.net>
2023-11-25 13:36:38 +0100

    Allow autologin by storing the user's password in encrypted storage

--- src/renderer/src/pages/LoginPage/index.tsx
+++ src/renderer/src/pages/LoginPage/index.tsx
@@ -7,12 +7,14 @@ import Autocomplete from '@mui/material/Autocomplete'
 import Snackbar from '@mui/material/Snackbar'
 import LoadingButton from '@mui/lab/LoadingButton'
 import useUserState from '../../state.ts'
+import { Checkbox, FormControlLabel, FormGroup } from '@mui/material'
 
 const KNOWN_STUDIP_HOSTS = ['https://studip.uni-osnabrueck.de/']
 
 function LoginPage() {
     const [loggingIn, setLoggingIn] = useState(false)
     const [successfullyLoggedIn, setSuccessfullyLoggedIn] = useState(false)
+    const [store_password, set_store_password] = useState(true)
 
     const set_logged_in = useUserState((state) => state.set_logged_in)
 
@@ -24,6 +26,7 @@ function LoginPage() {
                 window.localStorage.setItem('session_server', target.server.value)
                 window.localStorage.setItem('last_username', target.username.value)
                 window.localStorage.setItem('last_server', target.server.value)
+                if (store_password) IPC.encrypt_password(target.password.value)
                 set_logged_in(true)
                 setSuccessfullyLoggedIn(true)
                 setTimeout(() => setSuccessfullyLoggedIn(false), 5000)
@@ -51,6 +54,17 @@ function LoginPage() {
                             defaultValue={window.localStorage.getItem('last_server')}
                             renderInput={(params) => <TextField {...params} label='Server' name='server' required />}
                         />
+                        <FormGroup>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={store_password}
+                                        onChange={() => set_store_password(!store_password)}
+                                    />
+                                }
+                                label='Store password in secure storage'
+                            />
+                        </FormGroup>
                         <LoadingButton loading={loggingIn} type='submit' variant='contained' color='success'>
                             Login
                         </LoadingButton>
