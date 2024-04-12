9bbde05ca3c4f6bf952b69e0231429eca820a529
Mathis Dröge <mathis.droege@ewe.net>
2024-04-12 10:11:42 +0200

    Compact electron-vite config a bit

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,26 +1,25 @@
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react-swc'
+import type { BuildOptions } from 'vite'
 
-export default defineConfig(({ mode }) => ({
-    main: {
-        build: {
-            minify: mode === 'production',
-            sourcemap: mode === 'development' ? 'inline' : undefined
-        },
-        plugins: [externalizeDepsPlugin()]
-    },
-    preload: {
-        build: {
-            minify: mode === 'production',
-            sourcemap: mode === 'development' ? 'inline' : undefined
-        },
-        plugins: [externalizeDepsPlugin()]
-    },
-    renderer: {
-        build: {
-            minify: mode === 'production',
-            sourcemap: mode === 'development' ? 'inline' : undefined
-        },
-        plugins: [react()]
+export default defineConfig(({ mode }) => {
+    const commonBuildOptions: BuildOptions = {
+        minify: mode === 'production',
+        sourcemap: mode === 'development' ? 'inline' : undefined
     }
-}))
+
+    return {
+        main: {
+            build: commonBuildOptions,
+            plugins: [externalizeDepsPlugin()]
+        },
+        preload: {
+            build: commonBuildOptions,
+            plugins: [externalizeDepsPlugin()]
+        },
+        renderer: {
+            build: commonBuildOptions,
+            plugins: [react()]
+        }
+    }
+})
